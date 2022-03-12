2d4d8e7e5e21a80c6bb826b7c1bb3072739b8284
ewest8133 <ewwest01@gmail.com>
2022-03-11 19:19:20 -0700

    setup1

---
+++ src/main/java/cs455/scaling/Client.java
@@ -0,0 +1,4 @@
+package src.main.java.cs455.scaling;
+public class Client {
+    
+}
--- src/main/java/cs455/scaling/Main.java
+++ src/main/java/cs455/scaling/Main.java
@@ -1,24 +1,22 @@
-package cs455.overlay;
-
-import cs455.overlay.node.*;
+package src.main.java.cs455.scaling;
 
 public class Main {
     public static void main(String[] args) {
         try {
-            if (args[1].equals("registry")){
+            if (args[1].equals("server")){
                 String regName = args[1];
                 int port = Integer.parseInt(args[2]);
                 int numNodes = Integer.parseInt(args[3]);
                 int numMessages = Integer.parseInt(args[4]);
-                Registry registry = new Registry(regName, port, numNodes, numMessages);
-                registry.runRegistry();
+                //Registry registry = new Registry(regName, port, numNodes, numMessages);
+                //registry.runRegistry();
             }
-            else if (args[1].equals("messaging")) {
+            else if (args[1].equals("client")) {
                 //String nodeName = args[1];
                 String registryHost = args[2];
                 int registryPort = Integer.parseInt(args[3]);
-                MessagingNode messagingNode = new MessagingNode(registryHost, registryPort);
-                messagingNode.runNode();
+                //MessagingNode messagingNode = new MessagingNode(registryHost, registryPort);
+                //messagingNode.runNode();
             }
             else {
                 System.out.println("Error: Incorrect args\n Use 'registry [port] [numNodes]'' or 'messaging [hostName] [hostPort]'");
